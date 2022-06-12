03121ef0ed91bd9f34fda84638ff943c93d6362e
Mahyar Mohammadi <techguymahyar@gmail.com>
2022-06-12 20:44:59 +0430

    Add project files.

---
+++ AwaitablePattern/YieldAwaitable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwaitablePattern
+{
+    public struct YieldAwaitable
+    {
+        public YieldAwaiter GetAwaiter();
+
+        public struct YieldAwaiter : INotifyCompletion
+        {
+            public bool IsCompleted { get; private set; }
+            public void OnCompleted(Action continuation);
+            public void GetResult();
+
+        }
+    }
+}
